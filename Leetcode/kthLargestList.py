879bb9ee14df62e16d51da96d31ee5f2dc9fc911
Garrett Rodrigues <grodrigues@intellisis.com>
2015-07-09 13:59:04 -0700

    with randomness and correct paritioning

--- Leetcode/kthLargestList.py
+++ Leetcode/kthLargestList.py
@@ -5,6 +5,8 @@ Given [3,2,1,5,6,4] and k = 2, return 5.
 Do it O(n) time
     - a simple solution not meeting the above constraint would be to sort the list and then return the kth element from the end
 """
+import random
+
 class Solution:
     # @param {integer[]} nums
     # @param {integer} k
@@ -12,13 +14,15 @@ class Solution:
     def findKthLargest(self, nums, k):
         
         #return sorted(nums)[-k] cheater solution
-        
-        if len(nums) == 0:
-            return None
-        part = nums[0]
+        n = len(nums)
+        partInd = random.choice(range(n))
+        part = nums[partInd]
+        #swap em for easy partitioning
+        nums[partInd], nums[0] = nums[0], nums[partInd]
+
+        #because I'm lazy and don't want to partition in place
         smaller = []
-        bigger = []
-        
+        bigger = []        
         for i in range(1,len(nums)):
             if nums[i] >= part:
                 bigger += nums[i],
@@ -32,4 +36,5 @@ class Solution:
             return self.findKthLargest(bigger, k)
         else:
             return self.findKthLargest(smaller, k - nBigger - 1)
+            
             
