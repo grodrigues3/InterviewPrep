d1a477ca9e2586bcf2e9a489feb94d5f0fb6230d
Garrett Rodrigues <grodrigues@intellisis.com>
2015-07-09 13:54:22 -0700

    kth largest with missing randomness partition selection; works but should be fixed

---
+++ Leetcode/kthLargestList.py
@@ -0,0 +1,35 @@
+"""
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+Do it O(n) time
+    - a simple solution not meeting the above constraint would be to sort the list and then return the kth element from the end
+"""
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {integer}
+    def findKthLargest(self, nums, k):
+        
+        #return sorted(nums)[-k] cheater solution
+        
+        if len(nums) == 0:
+            return None
+        part = nums[0]
+        smaller = []
+        bigger = []
+        
+        for i in range(1,len(nums)):
+            if nums[i] >= part:
+                bigger += nums[i],
+            elif nums[i] < part:
+                smaller += nums[i],
+        
+        nBigger = len(bigger)
+        if nBigger == k-1:
+            return part
+        elif nBigger > k-1:
+            return self.findKthLargest(bigger, k)
+        else:
+            return self.findKthLargest(smaller, k - nBigger - 1)
+            
