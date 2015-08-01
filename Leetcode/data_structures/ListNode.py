1473a4ef125c5749758e4c5a510da5dae3499bef
Garrett Rodrigues <grodrigues@intellisis.com>
2015-07-31 17:23:11 -0700

    added delete duplicate and count islands

--- Leetcode/data_structures/ListNode.py
+++ Leetcode/data_structures/ListNode.py
@@ -3,3 +3,11 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+    def __str__(self):
+        toWrite = ""
+        cur = self
+        while cur:
+            toWrite += str(cur.val) + ", "
+            cur = cur.next
+        return toWrite[:-2]
